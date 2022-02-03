3d78f21132c68827572ebdbed5a9038d6424ec66
FinnJanssensEHB <71694845+FinnJanssensEHB@users.noreply.github.com>
2022-02-03 11:07:41 +0100

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,17 +1,17 @@
 const staticCacheName = 'site-static';
 const assets = [
-    './index.html',
-    './',
-    './main.js',
-    './style.css',
-    './images/trainworld-logo.jpg',
-    './images/TrainWorld_512.png',
-    './images/home_images/affiche.jpg',
-    './images/home_images/floor_plan.jpg',
-    './images/home_images/phoneexample.png',
-    './images/home_images/uniform.jpg',
+    '/index.html',
+    '/',
+    '/main.js',
+    '/style.css',
+    '/images/trainworld-logo.jpg',
+    '/images/TrainWorld_512.png',
+    '/images/home_images/affiche.jpg',
+    '/images/home_images/floor_plan.jpg',
+    '/images/home_images/phoneexample.png',
+    '/images/home_images/uniform.jpg',
     'https://use.typekit.net/ftc8abr.css',
-    './videos/Guido.mp4'
+    '/videos/Guido.mp4'
 ];
 
 self.addEventListener('install', e => {
