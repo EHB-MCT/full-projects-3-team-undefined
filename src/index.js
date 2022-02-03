7538bc8cf947eef55b9822421f6589491b48c177
FinnJanssensEHB <finn.janssens@student.ehb.be>
2022-02-03 13:16:59 +0100

    Update path to sw.js in script.js

--- src/index.js
+++ src/index.js
@@ -1,104 +1,104 @@
-"use strict"
+"use strict";
 
 import "../node_modules/@fortawesome/fontawesome-free/js/brands.js";
 import "../node_modules/@fortawesome/fontawesome-free/js/solid.js";
 import "../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js";
 
 // code to register service worker
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../docs/sw.js')
-        .then((registration) => console.log('Service worker registered', registration))
-        .catch((error) => console.log('Service worker not registered', error));
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./sw.js")
+    .then((registration) =>
+      console.log("Service worker registered", registration)
+    )
+    .catch((error) => console.log("Service worker not registered", error));
 }
 
 window.onload = function () {
-    getLanguage('nl');
-    showWelcomePopup();
-    initCloseButtons();
+  getLanguage("nl");
+  showWelcomePopup();
+  initCloseButtons();
 
-    // functions for the help modal
-    document.getElementById("helpBtn").addEventListener('click', e => {
-        document.getElementById("helpOverlay").style.display = 'inherit';
-        document.getElementById("helpPopup").style.display = 'flex';
-    })
+  // functions for the help modal
+  document.getElementById("helpBtn").addEventListener("click", (e) => {
+    document.getElementById("helpOverlay").style.display = "inherit";
+    document.getElementById("helpPopup").style.display = "flex";
+  });
 
-    document.getElementById("helpClose").addEventListener('click', e => {
-        closeHelpPopup();
-    })
+  document.getElementById("helpClose").addEventListener("click", (e) => {
+    closeHelpPopup();
+  });
 
-    document.getElementById('helpOverlay').addEventListener('click', e => {
-        closeHelpPopup();
-    })
+  document.getElementById("helpOverlay").addEventListener("click", (e) => {
+    closeHelpPopup();
+  });
 
-    // removing the content from the popups when one is closed
-    document.getElementById('popupClose').addEventListener("click", (e) => {
-        closePopups();
-    })
+  // removing the content from the popups when one is closed
+  document.getElementById("popupClose").addEventListener("click", (e) => {
+    closePopups();
+  });
 
-    let closeBtns = document.getElementsByClassName('popupBtn');
-    for (let btn of closeBtns) {
-        btn.addEventListener('click', e => {
-            closePopups();
-        })
+  let closeBtns = document.getElementsByClassName("popupBtn");
+  for (let btn of closeBtns) {
+    btn.addEventListener("click", (e) => {
+      closePopups();
+    });
+  }
+
+  document.getElementById("popupOverlay").addEventListener("click", (e) => {
+    closePopups();
+  });
+
+  //detect if a clickable element is clicked and open correct popup
+  document.getElementById("groundPlanSvg").addEventListener("click", (e) => {
+    const clickedObject = e.target.closest(".clickableObject");
+    if (clickedObject) {
+      const popupContainer = document.getElementById("popupContainer");
+      popupContainer.style.display = "inherit";
+      document.getElementById("popupOverlay").style.display = "inherit";
+      const objectName = clickedObject.getAttribute("data-name");
+      document.getElementById(`${objectName}Popup`).style.display = "flex";
+
+      if (objectName == "portrait") {
+        const guidoVideo = document.getElementById("guidoVideo");
+        guidoVideo.setAttribute("playsinline", true);
+        guidoVideo.play();
+      }
+
+      let pulseElement = document.getElementById(`${objectName}Pulse`);
+      if (pulseElement) {
+        pulseElement.setAttribute("stroke", "#eadec7");
+      }
+      popupContainer.removeEventListener("animationend", removeContent, true);
+      popupContainer.classList.remove("fade-out");
+      popupContainer.classList.add("fade-in");
     }
+  });
 
-    document.getElementById('popupOverlay').addEventListener("click", e => {
-        closePopups();
-    })
+  // when the switch is toggled, classes are added or removed
+  document.getElementById("darkmodeSlider").addEventListener("change", () => {
+    document.body.classList.toggle("darkmode");
+    document.getElementById("popupContainer").classList.toggle("darkmodePopup");
+    document.getElementById("askLandscape").classList.toggle("darkmode");
+    document.getElementById("helpPopup").classList.toggle("darkmodePopup");
+    document.getElementById("languages").classList.toggle("darkmode");
+  });
 
+  // change language menu when language is clicked
+  const buttonFR = document.getElementById("buttonFR");
+  const buttonNL = document.getElementById("buttonNL");
+  const buttonEN = document.getElementById("buttonEN");
 
+  async function getLanguage(dataLanguage) {
+    fetch(`languages/${dataLanguage}.json`)
+      .then((response) => response.json())
+      .then((data) => fillWithLanguage(data));
+  }
 
-    //detect if a clickable element is clicked and open correct popup
-    document.getElementById('groundPlanSvg').addEventListener("click", (e) => {
-        const clickedObject = e.target.closest('.clickableObject');
-        if (clickedObject) {
-            const popupContainer = document.getElementById("popupContainer")
-            popupContainer.style.display = 'inherit';
-            document.getElementById("popupOverlay").style.display = 'inherit';
-            const objectName = clickedObject.getAttribute('data-name');
-            document.getElementById(`${objectName}Popup`).style.display = 'flex';
-
-            if (objectName == "portrait") {
-                const guidoVideo = document.getElementById("guidoVideo");
-                guidoVideo.setAttribute('playsinline', true);
-                guidoVideo.play();
-            }
-
-            let pulseElement = document.getElementById(`${objectName}Pulse`);
-            if (pulseElement) {
-                pulseElement.setAttribute("stroke", "#eadec7")
-            }
-            popupContainer.removeEventListener("animationend", removeContent, true);
-            popupContainer.classList.remove('fade-out');
-            popupContainer.classList.add('fade-in');
-        }
-    })
-
-
-    // when the switch is toggled, classes are added or removed
-    document.getElementById('darkmodeSlider').addEventListener('change', () => {
-        document.body.classList.toggle("darkmode");
-        document.getElementById("popupContainer").classList.toggle("darkmodePopup");
-        document.getElementById("askLandscape").classList.toggle("darkmode");
-        document.getElementById('helpPopup').classList.toggle("darkmodePopup");
-        document.getElementById('languages').classList.toggle("darkmode");
-    })
-
-    // change language menu when language is clicked
-    const buttonFR = document.getElementById("buttonFR");
-    const buttonNL = document.getElementById("buttonNL");
-    const buttonEN = document.getElementById("buttonEN");
-
-    async function getLanguage(dataLanguage) {
-        fetch(`languages/${dataLanguage}.json`)
-            .then(response => response.json())
-            .then(data => fillWithLanguage(data));
-    }
-
-    function fillWithLanguage(dataLanguage) {
-        //change popup mainpage language
-        let containerRoom = document.getElementById('popupContainer');
-        containerRoom.innerHTML = `
+  function fillWithLanguage(dataLanguage) {
+    //change popup mainpage language
+    let containerRoom = document.getElementById("popupContainer");
+    containerRoom.innerHTML = `
         <div id="popupClose" class="closeBtn"><i class="fas fa-times"></i></div>
         <div id="phonePopup" class="popupContent">
             <div class="popupImgContainer">
@@ -145,142 +145,140 @@ window.onload = function () {
                 <button class="popupBtn">${dataLanguage.backLiving_text}</button>
             </div>
         </div>
-        `
-        //change intro popup language
-        let containerPopup = document.getElementById('welcomePopup');
-        containerPopup.innerHTML = `
+        `;
+    //change intro popup language
+    let containerPopup = document.getElementById("welcomePopup");
+    containerPopup.innerHTML = `
         <div id="welcomeClose" class="closeBtn"><i class="fas fa-times"></i></div>
         <h2>${dataLanguage.popup_title}</h2>
         <p>${dataLanguage.popup_text}</p>
         <button id="welcomeBtn" class="popupBtn">${dataLanguage.popup_button}</button>
         `;
 
-        // change help popup language
-        let helpPopup = document.getElementById('helpPopup');
-        helpPopup.innerHTML = `
+    // change help popup language
+    let helpPopup = document.getElementById("helpPopup");
+    helpPopup.innerHTML = `
         <div id="helpClose" class="closeBtn"><i class="fas fa-times"></i></div>
         <h2>${dataLanguage.help_title}</h2>
         <p>${dataLanguage.help_text1}</p>
         <p>${dataLanguage.help_text2}</p>`;
 
-        //Extra changes
-        document.getElementById('turnScreen').innerHTML = dataLanguage.main_turnScreen;
-        //document.getElementById('darkmodeLabel').innerHTML = dataLanguage.main_darkmodeButton;
-        initCloseButtons();
+    //Extra changes
+    document.getElementById("turnScreen").innerHTML =
+      dataLanguage.main_turnScreen;
+    //document.getElementById('darkmodeLabel').innerHTML = dataLanguage.main_darkmodeButton;
+    initCloseButtons();
+  }
+
+  function initCloseButtons() {
+    // removing the content from the popups when one is closed
+    document.getElementById("popupClose").addEventListener("click", (e) => {
+      closePopups();
+    });
+
+    let closeBtns = document.getElementsByClassName("popupBtn");
+    for (let btn of closeBtns) {
+      btn.addEventListener("click", (e) => {
+        closePopups();
+      });
     }
-
-    function initCloseButtons() {
-        // removing the content from the popups when one is closed
-        document.getElementById('popupClose').addEventListener("click", (e) => {
-            closePopups();
-        })
-
-        let closeBtns = document.getElementsByClassName('popupBtn');
-        for (let btn of closeBtns) {
-            btn.addEventListener('click', e => {
-                closePopups();
-            })
-        }
-        //intro popup buttons  
-        const exitPopup = document.getElementById("welcomeClose");
-        const welcomeBtn = document.getElementById("welcomeBtn");
-        exitPopup.addEventListener("click", () => {
-            closeWelcomePopup();
-        });
-        welcomeBtn.addEventListener("click", () => {
-            closeWelcomePopup();
-        });
-
-        document.getElementById("helpClose").addEventListener('click', e => {
-            closeHelpPopup();
-        })
-    }
-
-    buttonNL.addEventListener('click', e => {
-        getLanguage('nl');
-        buttonNL.style["border-bottom"] = "3px solid #fc4c00";
-        buttonNL.style["padding-bottom"] = "0";
-        buttonFR.style["border-bottom"] = "none";
-        buttonFR.style["padding-bottom"] = "3px";
-        buttonEN.style["border-bottom"] = "none";
-        buttonEN.style["padding-bottom"] = "3px";
-    });
-
-    buttonFR.addEventListener('click', e => {
-        getLanguage('fr');
-        buttonFR.style["border-bottom"] = "3px solid #fc4c00";
-        buttonFR.style["padding-bottom"] = "0";
-        buttonNL.style["border-bottom"] = "none";
-        buttonNL.style["padding-bottom"] = "3px";
-        buttonEN.style["border-bottom"] = "none";
-        buttonEN.style["padding-bottom"] = "3px";
-    });
-
-    buttonEN.addEventListener('click', e => {
-        getLanguage('en');
-        buttonEN.style["border-bottom"] = "3px solid #fc4c00";
-        buttonEN.style["padding-bottom"] = "0";
-        buttonFR.style["border-bottom"] = "none";
-        buttonFR.style["padding-bottom"] = "3px";
-        buttonNL.style["border-bottom"] = "none";
-        buttonNL.style["padding-bottom"] = "3px";
-    });
-
-}
-
-
-function showWelcomePopup() {
-    const popup = document.getElementById("welcomePopup");
-    popup.style.display = "flex";
-
-    // when the cross is clicked, the popup disappears
+    //intro popup buttons
     const exitPopup = document.getElementById("welcomeClose");
     const welcomeBtn = document.getElementById("welcomeBtn");
     exitPopup.addEventListener("click", () => {
-        closeWelcomePopup();
+      closeWelcomePopup();
     });
     welcomeBtn.addEventListener("click", () => {
-        closeWelcomePopup();
+      closeWelcomePopup();
     });
+
+    document.getElementById("helpClose").addEventListener("click", (e) => {
+      closeHelpPopup();
+    });
+  }
+
+  buttonNL.addEventListener("click", (e) => {
+    getLanguage("nl");
+    buttonNL.style["border-bottom"] = "3px solid #fc4c00";
+    buttonNL.style["padding-bottom"] = "0";
+    buttonFR.style["border-bottom"] = "none";
+    buttonFR.style["padding-bottom"] = "3px";
+    buttonEN.style["border-bottom"] = "none";
+    buttonEN.style["padding-bottom"] = "3px";
+  });
+
+  buttonFR.addEventListener("click", (e) => {
+    getLanguage("fr");
+    buttonFR.style["border-bottom"] = "3px solid #fc4c00";
+    buttonFR.style["padding-bottom"] = "0";
+    buttonNL.style["border-bottom"] = "none";
+    buttonNL.style["padding-bottom"] = "3px";
+    buttonEN.style["border-bottom"] = "none";
+    buttonEN.style["padding-bottom"] = "3px";
+  });
+
+  buttonEN.addEventListener("click", (e) => {
+    getLanguage("en");
+    buttonEN.style["border-bottom"] = "3px solid #fc4c00";
+    buttonEN.style["padding-bottom"] = "0";
+    buttonFR.style["border-bottom"] = "none";
+    buttonFR.style["padding-bottom"] = "3px";
+    buttonNL.style["border-bottom"] = "none";
+    buttonNL.style["padding-bottom"] = "3px";
+  });
+};
+
+function showWelcomePopup() {
+  const popup = document.getElementById("welcomePopup");
+  popup.style.display = "flex";
+
+  // when the cross is clicked, the popup disappears
+  const exitPopup = document.getElementById("welcomeClose");
+  const welcomeBtn = document.getElementById("welcomeBtn");
+  exitPopup.addEventListener("click", () => {
+    closeWelcomePopup();
+  });
+  welcomeBtn.addEventListener("click", () => {
+    closeWelcomePopup();
+  });
 }
 
 function closeWelcomePopup() {
-    const popup = document.getElementById("welcomePopup");
-    popup.style.display = "none";
-    document.getElementById('homepage').style.display = "flex";
+  const popup = document.getElementById("welcomePopup");
+  popup.style.display = "none";
+  document.getElementById("homepage").style.display = "flex";
 }
 
 function closeHelpPopup() {
-    const popup = document.getElementById('helpPopup');
-    popup.style.display = "none";
-    document.getElementById('helpOverlay').style.display = 'none';
+  const popup = document.getElementById("helpPopup");
+  popup.style.display = "none";
+  document.getElementById("helpOverlay").style.display = "none";
 }
 
 function closePopups() {
-    const popupContainer = document.getElementById("popupContainer")
-    popupContainer.classList.remove('fade-in');
-    popupContainer.classList.add('fade-out');
+  const popupContainer = document.getElementById("popupContainer");
+  popupContainer.classList.remove("fade-in");
+  popupContainer.classList.add("fade-out");
 
-    popupContainer.addEventListener("animationend", removeContent, true);
+  popupContainer.addEventListener("animationend", removeContent, true);
 
-    const popupTexts = document.getElementsByClassName('popupText');
-    for (let text of popupTexts) {
-        text.scrollTop = 0;
-    }
+  const popupTexts = document.getElementsByClassName("popupText");
+  for (let text of popupTexts) {
+    text.scrollTop = 0;
+  }
 
+  const popupContents = document.getElementsByClassName("popupContent");
+  for (let content of popupContents) {
+    content.style.display = "none";
+  }
 
-    const popupContents = document.getElementsByClassName('popupContent');
-    for (let content of popupContents) {
-        content.style.display = "none";
-    }
-
-    const guidoVideo = document.getElementById("guidoVideo");
-    guidoVideo.pause();
-    guidoVideo.currentTime = 0;
-    guidoVideo.load();
+  const guidoVideo = document.getElementById("guidoVideo");
+  guidoVideo.pause();
+  guidoVideo.currentTime = 0;
+  guidoVideo.load();
 }
 
 function removeContent() {
-    popupContainer.style.display = "none";
-    document.getElementById('popupOverlay').style.display = 'none';
+  popupContainer.style.display = "none";
+  document.getElementById("popupOverlay").style.display = "none";
 }
